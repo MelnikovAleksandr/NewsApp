75f8ef9873a8605c4c540e0ccde2ec47db706b86
Your Name <melnikov150990@mail.ru>
2023-01-01 14:56:40 +0500

    No Fragment Splash Screen

--- app/src/main/java/ru/asmelnikov/android/newsapp/MainActivity.kt
+++ app/src/main/java/ru/asmelnikov/android/newsapp/MainActivity.kt
@@ -2,8 +2,14 @@ package ru.asmelnikov.android.newsapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
@@ -11,28 +17,21 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import ru.asmelnikov.android.newsapp.databinding.ActivityMainBinding
+import ru.asmelnikov.android.newsapp.ui.favorite.FavoriteFragment
+import ru.asmelnikov.android.newsapp.ui.main.MainFragment
+import ru.asmelnikov.android.newsapp.ui.search.SearchFragment
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private var _binding: ActivityMainBinding? = null
-    private val nBinding get() = _binding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_splash)
-        CoroutineScope(Dispatchers.Main).launch {
-            delay(5000)
-            _binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(nBinding?.root)
-            bottom_nav_menu.setupWithNavController(
-                navController = nav_host_fragment.findNavController()
-            )
-        }
-    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
+        setTheme(R.style.Theme_NewsApp)
+        setContentView(R.layout.activity_main)
+
+        bottom_nav_menu.setupWithNavController(
+            navController = nav_host_fragment.findNavController()
+        )
     }
 }
